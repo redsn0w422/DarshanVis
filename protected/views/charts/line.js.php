0c2aa4d3948d37506d163e1cf4f5bce857ed8212
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-09-16 11:26:38 -0500

    finished task

--- protected/views/charts/line.js.php
+++ protected/views/charts/line.js.php
@@ -14,53 +14,50 @@ $series_str = rtrim($series_str, ",");
 ?>
 
 <script type="text/javascript">
-    $(function() {
-        $('#chart-container').highcharts({
-            chart: {
-                type: 'line',
-                zoomType: 'xy'
-            },
-            title: {
-                text: '<?php echo $chart["title"] ?>',
-                x: -20 //center
-            },
-            subtitle: {
+$(function(){
+    $("#chart-container").highcharts({
+        title: {
+            text: '<?php echo $chart["title"] ?>',
+            x: -20 //center
+        },
+        subtitle: {
                 text: '<?php echo $chart["subtitle"] ?>',
                 x: -20
+        },
+        xAxis: {
+            title: {
+                enabled: true,
+                text: '<?php echo $chart["xAxis"]["title"] ?>'
             },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: '<?php echo $chart["xAxis"]["title"] ?>'
-                },
-                categories: [<?php echo $cat_str; ?>]
+            categories: [<?php echo $cat_str; ?>]
 
-            },
-            yAxis: {
-                title: {
+        },
+        yAxis: {
+            title: {
                     text: '<?php echo $chart["yAxis"]["title"] ?>'
-                },
-                plotLines: [{
+            },
+            type: '<?php echo $chart["yAxis"]["type"] ?>',
+            // minorTickInterval: 100
+            // you'd have to add a type pass through
+            // to the rest of the pages that use line charts
+            plotLines: [{
                         value: 0,
                         width: 5,
                         color: '#808080'
-                    }],
-                min: 0
-            },
-            tooltip: {
-                valueSuffix: ''
-            },
-            legend: {
+            }]
+            // min: 0 <- breaks the chart
+        },
+        legend: {
                 layout: 'vertical',
                 align: 'right',
                 verticalAlign: 'middle',
                 borderWidth: 0,
                 enabled: false 
-            },
-            series: [{
-                    name: '<?php echo $chart["series"][0]["name"]; ?>',
-                    data: [<?php echo $series_str ?>]
-                }]
-        });
+        },
+        series: [{
+            name: '<?php echo $chart["series"][0]["name"]; ?>',
+            data: [<?php echo $series_str ?>]
+        }]
     });
+});
 </script>
