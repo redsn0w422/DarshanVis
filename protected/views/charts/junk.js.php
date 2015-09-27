71b0ea0c140606c84f8f9949b81843e1eaf12d5e
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-09-27 13:56:53 -0500

    adding to github

---
+++ protected/views/charts/junk.js.php
@@ -0,0 +1,20 @@
+ // , 
+                // {
+                //     name: '<?php echo $chart["series"][0]["series3-name"] ?>',
+                //     color: 'rgba(119, 152, 191, .5)',
+                //     data: [<?php echo $series2_str; ?>]
+                // }, 
+                // {
+                //     name: '<?php echo $chart["series"][0]["series4-name"] ?>',
+                //     color: 'rgba(119, 152, 191, .5)',
+                //     data: [<?php echo $series2_str; ?>]
+                // }
+
+
+
+
+                , {
+                    name: '<?php echo $chart["series"][0]["series2-name"] ?>',
+                    color: 'rgba(119, 152, 191, .5)',
+                    data: [<?php echo $series2_str; ?>]
+                }
--- protected/views/charts/scatter.js.php
+++ protected/views/charts/scatter.js.php
@@ -10,15 +10,15 @@ foreach ($data as $d) {
 //    $series1_str .= '[' . $d[$chart["series"][0]["xaxis"]] . ',' . $d[$chart["series"][0]["series1"]] . '],';
 //    $series2_str .= '[' . $d[$chart["series"][0]["xaxis"]] . ',' . $d[$chart["series"][0]["series2"]] . '],';
     $series1_str .= '[' . $index . ',' . $d[$chart["series"][0]["series1"]] . '],';
-    $series2_str .= '[' . $index . ',' . $d[$chart["series"][0]["series2"]] . '],';
-    $series3_str .= '[' . $index . ',' . $d[$chart["series"][0]["series3"]] . '],';
-    $series4_str .= '[' . $index . ',' . $d[$chart["series"][0]["series4"]] . '],';
+    // $series2_str .= '[' . $index . ',' . $d[$chart["series"][0]["series2"]] . '],';
+    // $series3_str .= '[' . $index . ',' . $d[$chart["series"][0]["series3"]] . '],';
+    // $series4_str .= '[' . $index . ',' . $d[$chart["series"][0]["series4"]] . '],';
     $index++;
 }
 $series1_str = rtrim($series1_str, ",");
-$series2_str = rtrim($series2_str, ",");
-$series3_str = rtrim($series3_str, ",");
-$series4_str = rtrim($series4_str, ",");
+// $series2_str = rtrim($series2_str, ",");
+// $series3_str = rtrim($series3_str, ",");
+// $series4_str = rtrim($series4_str, ",");
 ?>
 <script type="text/javascript">
     $(function() {
@@ -89,18 +89,6 @@ $series4_str = rtrim($series4_str, ",");
                     color: 'rgba(223, 83, 83, .5)',
                     data: [<?php echo $series1_str ?>]
 
-                }, {
-                    name: '<?php echo $chart["series"][0]["series2-name"] ?>',
-                    color: 'rgba(119, 152, 191, .5)',
-                    data: [<?php echo $series2_str; ?>]
-                }, {
-                    name: '<?php echo $chart["series"][0]["series3-name"] ?>',
-                    color: 'rgba(119, 152, 191, .5)',
-                    data: [<?php echo $series2_str; ?>]
-                }, {
-                    name: '<?php echo $chart["series"][0]["series4-name"] ?>',
-                    color: 'rgba(119, 152, 191, .5)',
-                    data: [<?php echo $series2_str; ?>]
                 }
                 
                 ]
